9944357cc8f7c2b58d39c5fb0213d3dc90da80db
Rawand <rawandhawiz@gmail.com>
2016-03-22 12:26:11 +0000

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ sudo pip install -r requirements.txt
 sudo python manage.py makemigrations api
 sudo python manage.py migrate
 sudo /opt/bitnami/ctlscript.sh restart apache
-sudo redis-server
+sudo redis-server --daemonize yes
