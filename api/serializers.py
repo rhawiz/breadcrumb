9084043373ca75ed7e955783de72064b5be1e4d5
Rawand <rawandhawiz@gmail.com>
2016-04-07 15:05:18 +0100

    permission issues

--- api/serializers.py
+++ api/serializers.py
@@ -256,7 +256,7 @@ class UploadImageSerializer(serializers.ModelSerializer):
         if not os.path.exists(file_path):
             os.mkdir(file_path)
         name = validated_data.get('name') or file_name.split(".")[0]
-        fh = open("{}\\{}".format(file_path, file_name), "wb")
+        fh = open("{}/{}".format(file_path, file_name), "wb")
         fh.write(image_base64.decode('base64'))
         fh.close()
         url = "{}users/{}/{}".format(settings.MEDIA_URL, user_profile.id, file_name)
