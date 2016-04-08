520c74ac27d38e1f29347c9022f7c5d6356daba9
Rawand <rawandhawiz@gmail.com>
2016-04-08 18:26:43 +0100

    Usercontent output

--- api/models.py
+++ api/models.py
@@ -264,3 +264,6 @@ class UserContent(models.Model):
     extra_data = JSONField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "{} - {}".format(self.user.user.username, self.source)
