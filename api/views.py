5bc1ee556006e5c3e1d26a8c543f51710391e7c5
Rawand <rawandhawiz@gmail.com>
2016-04-10 10:47:03 +0100

    session key storage

--- api/views.py
+++ api/views.py
@@ -145,11 +145,12 @@ class TwitterCallback(APIView):
         # data['s'] = s
         # data['s.session_key'] = s.session_key
         # return Response(data=data)
-        request_token = s['request_token']
+        request_token = s.get('request_token')
         data = {
             'oauth_verifier': request.GET['oauth_verifier'],
             'request_token': request_token,
         }
+        return Response(data=data)
         s.delete('request_token')
         s.save()
         return Response(data=data)
